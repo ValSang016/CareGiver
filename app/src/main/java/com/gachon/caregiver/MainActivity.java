c4a66519c6063bc4ccffcc72a9435ae53b9343ef
sangheon <tkdgjs1315@naver.com>
2023-05-02 14:33:01 +0900

    Second_Debugging_Success_Sangheon

--- app/src/main/java/com/gachon/caregiver/MainActivity.java
+++ app/src/main/java/com/gachon/caregiver/MainActivity.java
@@ -1,10 +1,12 @@
 package com.gachon.caregiver;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button login_bt = (Button) findViewById(R.id.login_button);//로그인 버튼 연결
-        Button find_pw = (Button) findViewById(R.id.find_password);//비밀번호 찾기 연결
-        Button some_id = (Button) findViewById(R.id.some_id); //회원가입 버튼 연결
+        Button login_bt = findViewById(R.id.login_button);//로그인 버튼 연결
+        Button find_pw = findViewById(R.id.find_password);//비밀번호 찾기 연결
+        Button some_id = findViewById(R.id.some_id); //회원가입 버튼 연결
 
         login_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+//                startActivity(intent);
             }
         });
 
