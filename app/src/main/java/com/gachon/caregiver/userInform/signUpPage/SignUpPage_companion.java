8c62a21299c349179cede7d8ef01769ef2f9caaf
sangheon <tkdgjs1315@naver.com>
2023-05-28 03:49:44 +0900

    무조건 올려버렷_Sangheon

--- app/src/main/java/com/gachon/caregiver/userInform/signUpPage/SignUpPage_companion.java
+++ app/src/main/java/com/gachon/caregiver/userInform/signUpPage/SignUpPage_companion.java
@@ -2,6 +2,8 @@ package com.gachon.caregiver.userInform.signUpPage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +15,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.gachon.caregiver.R;
-import com.gachon.caregiver.connect_server_data;
 import com.gachon.caregiver.userInform.loginPage.LoginPage;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,12 +22,12 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpPage_companion extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
-    private connect_server_data connectServerData;
     String name;
     String birth;
     String gender;
@@ -34,6 +35,7 @@ public class SignUpPage_companion extends AppCompatActivity {
     String sign_up_pw;
     String phone_number;
 
+    isOkSignUP allGood = new isOkSignUP();
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_companion);
@@ -51,9 +53,71 @@ public class SignUpPage_companion extends AppCompatActivity {
         RadioButton sign_up_gender_m = findViewById(R.id.male);
         EditText make_up_id = findViewById(R.id.id_write);
         EditText make_up_pw = findViewById(R.id.password_write);
+        EditText make_up_pw_r = findViewById(R.id.password_ok);
         EditText sign_up_phone_number = findViewById(R.id.phonenumber_write);
 
-        connectServerData = new connect_server_data();
+//      TextView valid = findViewById(R.id.validEmail);
+
+        //이메일이 양식에 맞게 작성되는지
+        make_up_id.addTextChangedListener(new TextWatcher() {
+            SignUpChecking checking = new SignUpChecking();
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String emailNeedToCheck = make_up_id.getText().toString();
+                //아직은 안됨
+                if(checking.isEmailValid(emailNeedToCheck)){
+                    Toast.makeText(SignUpPage_companion.this, "이메일 양식이 적절합니다.",
+                            Toast.LENGTH_SHORT).show();
+                    allGood.setEmailGreat(true);
+                } else {
+//                    validEmail.setText("이메일 양식이 부적적합니다");
+//                    validEmail.setTextColor(Color.RED);
+                    Toast.makeText(SignUpPage_companion.this, "이메일 양식이 부적절합니다.",
+                            Toast.LENGTH_SHORT).show();
+                    allGood.setEmailGreat(false);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+
+        make_up_pw_r.addTextChangedListener(new TextWatcher() {
+            SignUpChecking checking = new SignUpChecking();
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String ps1 = make_up_pw.getText().toString();
+                String ps2 = make_up_pw_r.getText().toString();
+                if(checking.isPasswordConfirmed(ps1, ps2)){
+                    Toast.makeText(SignUpPage_companion.this, "비밀번호가 일치합니다.",
+                            Toast.LENGTH_SHORT).show();
+                    allGood.setPwGreat(true);
+                } else {
+                    Toast.makeText(SignUpPage_companion.this, "비밀번호가 일치하지 않습니다.",
+                            Toast.LENGTH_SHORT).show();
+                    allGood.setPwGreat(false);
+                }
+            }
+        });
+
 
         //성별 체크후 성별 보내주는 곳
         check_gender_group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -77,8 +141,6 @@ public class SignUpPage_companion extends AppCompatActivity {
 
         //다음 버튼을 눌렀을 경우 다시 로그인 창으로 넘어갈 수 있게 하는 코드이다
         next_bt.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
             public void onClick(View view) {
 
@@ -88,19 +150,28 @@ public class SignUpPage_companion extends AppCompatActivity {
                 sign_up_pw = make_up_pw.getText().toString();
                 phone_number = sign_up_phone_number.getText().toString();
 
-//                connectServerData.sign_up_companion_connect(name,birth,gender,sign_up_id,sign_up_pw,phone_number);
-
-                signUp(sign_up_id, sign_up_pw, name, birth, gender, phone_number);  //파이어베이스 회원가입 메서드
-
-
+                try {
+                    if(allGood.okSignUp()) {
+                        signUp(sign_up_id, sign_up_pw, name, birth, gender, phone_number, 0);  //파이어베이스 회원가입 메서드
+                    } else{
+                        Toast.makeText(SignUpPage_companion.this, "다시 입력해주세요.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e){
+                    Toast.makeText(SignUpPage_companion.this, "다시 입력해주세요.",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
+
+
     private DatabaseReference mDatabase;
     // 회원가입 버튼 클릭 시 호출되는 메서드
-    private void signUp(String email, String password, String username, String birth, String gender, String phoneNumber) {
-        Intent login = new Intent(getApplicationContext(), LoginPage.class);
+    private void signUp(String email, String password, String username, String birth, String gender, String phoneNumber, Integer userTP) {
+        Intent login = new Intent(SignUpPage_companion.this, LoginPage.class);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -110,8 +181,8 @@ public class SignUpPage_companion extends AppCompatActivity {
                             FirebaseUser user = mAuth.getCurrentUser();
                             String userId = user.getUid();
                             // 추가적인 사용자 정보 저장하거나 초기화 작업
-                            UserInformation userInfo = new UserInformation(username, birth, gender, phoneNumber);
-                            mDatabase.child("users").child(userId).setValue(userInfo);
+                            UserInformation userInfo = new UserInformation(email, password, username, birth, gender, phoneNumber, userTP);
+                            mDatabase.child("Users").child("UID").child(userId).setValue(userInfo);
 
                             // 회원가입 후에 다음 화면으로 이동하거나 액션이 필요한 경우에 대한 코드를 작성합니다.
                             startActivity(login);
