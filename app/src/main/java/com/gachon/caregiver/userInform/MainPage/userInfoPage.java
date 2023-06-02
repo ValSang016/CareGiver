beb07ecc8b250700c6b7aef02b38525f146ab282
yuheun <youheun422@gmail.com>
2023-06-02 21:07:08 +0900

    추천 비추천 버튼 (아직 두번 클릭 안되는 건 안함)

--- app/src/main/java/com/gachon/caregiver/userInform/MainPage/userInfoPage.java
+++ app/src/main/java/com/gachon/caregiver/userInform/MainPage/userInfoPage.java
@@ -1,7 +1,13 @@
 package com.gachon.caregiver.userInform.MainPage;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +20,11 @@ public class userInfoPage extends AppCompatActivity {
     private EditText address;
     private EditText special;
 
+    private TextView tvCount;
+    private Button upBtn;
+    private Button downBtn;
+    private int count = 0;
+
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.myinformation_companion);
@@ -23,6 +34,30 @@ public class userInfoPage extends AppCompatActivity {
         address = findViewById(R.id.address);
         special = findViewById(R.id.significant);
 
+        upBtn = findViewById(R.id.btn_add);
+        downBtn = findViewById(R.id.btn_minus);
+        tvCount = findViewById(R.id.tv_count);
+        upBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: upBtn : " + view.getClass().getName());
+                count++;
+                tvCount.setText(count + "");
+            }
+        });
 
+        downBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    count--;
+                    tvCount.setText(count + "");
+            }
+        });
+
+
+        // 이제 보호자가 특정 동행인에 대해 추천 or 비추천 버튼 눌렀으면
+        // 버튼을 비활성화 하거나 안보이게 하는 코드 추가해야되는데
+        // 내가 원하는 걸 찾기 어려워서 찾은게 시간에 따라 버튼 중복 클릭이 안되게 하는 게 있는데
+        // 그거 야매로 쓰는 방법도 염두에 두는 중
     }
 }
