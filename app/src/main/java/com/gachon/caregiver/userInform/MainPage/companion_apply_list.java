1cd6e8865388ab2b26ef1e17cb2f1288a93acf1b
yuheun <youheun422@gmail.com>
2023-06-04 03:33:04 +0900

    companion_apply_list

--- app/src/main/java/com/gachon/caregiver/userInform/MainPage/companion_apply_list.java
+++ app/src/main/java/com/gachon/caregiver/userInform/MainPage/companion_apply_list.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
@@ -19,10 +20,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.gachon.caregiver.R;
+import com.gachon.caregiver.userInform.Manager;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -36,40 +40,74 @@ public class companion_apply_list extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.companion_apply_list);
 
-        createButtonsForUsers();
+        // Firebase 데이터베이스 참조
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
 
+        // Firebase 데이터베이스에서 사용자 데이터 읽기
+        readUserData();
     }
 
-    // 데이터베이스에서 저장된 user id 목록을 가져오는 메소드
-    private List<String> getUserIdsFromDatabase() {
-        List<String> userIds = new ArrayList<>();
-        userIds.add("user1");
-        userIds.add("user2");
-        userIds.add("user3");
-        return userIds;
-    }
-
-    private void createButtonsForUsers() {
-        List<String> userIds = getUserIdsFromDatabase();
-
-        // 버튼을 생성할 레이아웃
-        LinearLayout buttonLayout = findViewById(R.id.buttonLayout);
-
-        for (String userId : userIds) {
-            Button button = new Button(this);
-            button.setText("User: " + userId);
-
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent send_apply = new Intent(companion_apply_list.this, Manager_btn_clicked.class);
-                    startActivity(send_apply);
+    private void readUserData() {
+        // 데이터베이스에서 사용자 데이터 읽기
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 사용자 데이터 목록 가져오기
+                List<User> userList = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    User user = snapshot.getValue(User.class);
+                    userList.add(user);
                 }
-            });
+                // 사용자 데이터를 화면에 표시
+                displayUserData(userList);
+            }
 
-            // 생성한 버튼을 레이아웃에 추가
-            buttonLayout.addView(button);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 읽기 실패 시 처리
+                Toast.makeText(companion_apply_list.this, "데이터를 읽어올 수 없습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void displayUserData(List<User> userList) {
+        LinearLayout userListLayout = findViewById(R.id.user_item);
+
+        for (User user : userList) {
+            String approvalText;
+            switch (user.getApproval()) {
+                case 0:
+                    approvalText = "대기중";
+                    break;
+                case 1:
+                    approvalText = "승인됨";
+                    break;
+                case 2:
+                    approvalText = "거부됨";
+                    break;
+                default:
+                    approvalText = "알 수 없음";
+            }
+
+            TextView nameTextView = findViewById(R.id.nameTextView);
+            TextView approvalTextView = findViewById(R.id.approvalTextView);
+
+            nameTextView.setText(user.getEmail());
+            approvalTextView.setText(approvalText);
         }
     }
 
+    // User 클래스 예시 (사용자 데이터 구조에 맞게 수정해야 함)
+    private static class User {
+        private String email;
+        private int approval;
+
+        public String getEmail() {
+            return email;
+        }
+
+        public int getApproval() {
+            return approval;
+        }
+    }
 }
