137dab1d75292a610e3b57448be2a37f55e5f7e4
parkjonghyun <hynmail@naver.com>
2023-06-03 22:31:40 +0900

    make calender store

--- app/src/main/java/com/gachon/caregiver/userInform/MainPage/CalendarCompanionActivity.java
+++ app/src/main/java/com/gachon/caregiver/userInform/MainPage/CalendarCompanionActivity.java
@@ -1,6 +1,9 @@
 package com.gachon.caregiver.userInform.MainPage;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -13,8 +16,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.gachon.caregiver.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -28,15 +36,22 @@ public class CalendarCompanionActivity extends AppCompatActivity {
     public Button cha_Btn, del_Btn, save_Btn;
     public TextView diaryTextView, textView2, textView3;
     public EditText contextEditText;
-
+    private FirebaseAuth mAuth;
     private FirebaseFirestore firestore;
+    private int store_year;
+    private int store_month;
+    private int store_day;
+    private DatabaseReference databaseReference;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_companion);
 
+        mAuth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
         calendarView = findViewById(R.id.calendarView);
         diaryTextView = findViewById(R.id.diaryTextView);
@@ -57,6 +72,9 @@ public class CalendarCompanionActivity extends AppCompatActivity {
                 cha_Btn.setVisibility(View.INVISIBLE);
                 del_Btn.setVisibility(View.INVISIBLE);
                 diaryTextView.setText(String.format("%d / %d / %d", year, month + 1, dayOfMonth));
+                store_day = dayOfMonth;
+                store_month = month;
+                store_year = year;
                 contextEditText.setText("");
                 checkDay(year, month, dayOfMonth);
             }
@@ -65,7 +83,7 @@ public class CalendarCompanionActivity extends AppCompatActivity {
         save_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                saveDiary(readDay);
+
                 str = contextEditText.getText().toString();
                 textView2.setText(str);
                 save_Btn.setVisibility(View.INVISIBLE);
@@ -74,17 +92,31 @@ public class CalendarCompanionActivity extends AppCompatActivity {
                 contextEditText.setVisibility(View.INVISIBLE);
                 textView2.setVisibility(View.VISIBLE);
 
-                Map<String, Object> data = new HashMap<>();
-                data.put("diary", str);
+                String context = str;
 
-                firestore.collection("diaries").document(readDay)
-                        .set(data)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Toast.makeText(CalendarCompanionActivity.this, "일정이 저장되었습니다.", Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                Map<String, Object> data = new HashMap<>();
+                data.put("context", context);
+                data.put("day", store_day);
+                data.put("month", store_month+1);
+                data.put("year", store_year);
+
+                final FirebaseUser user = mAuth.getCurrentUser();
+                if (user != null) {
+                    databaseReference.child("calenders").child(user.getUid()).setValue(data)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(CalendarCompanionActivity.this, "달력이 업로드되었습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(CalendarCompanionActivity.this, "달력 업로드에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+
             }
         });
 
@@ -94,7 +126,6 @@ public class CalendarCompanionActivity extends AppCompatActivity {
                 contextEditText.setVisibility(View.VISIBLE);
                 textView2.setVisibility(View.INVISIBLE);
                 contextEditText.setText(str);
-
                 save_Btn.setVisibility(View.VISIBLE);
                 cha_Btn.setVisibility(View.INVISIBLE);
                 del_Btn.setVisibility(View.INVISIBLE);
@@ -118,6 +149,10 @@ public class CalendarCompanionActivity extends AppCompatActivity {
 
     public void checkDay(int cYear, int cMonth, int cDay) {
         readDay = "" + cYear + "-" + (cMonth + 1) + "-" + cDay + ".txt";
+
+        store_day = cDay;
+        store_month = cMonth;
+        store_year = cYear;
 
         str = "";
         textView2.setVisibility(View.INVISIBLE);
@@ -163,30 +198,27 @@ public class CalendarCompanionActivity extends AppCompatActivity {
 
     public void removeDiary(String readDay) {
         Map<String, Object> data = new HashMap<>();
-        data.put("diary", "");
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String getemail = currentUser.getEmail();
 
-        firestore.collection("diaries").document(readDay)
+        String email = getemail;
+
+        String day = String.valueOf(store_day);
+        String month = String.valueOf(store_month);
+        String year = String.valueOf(store_year);
+
+        data.put("context", str);
+        data.put("day", day);
+        data.put("email", email);
+        data.put("month", month);
+        data.put("year", year);
+
+        firestore.collection("calender").document(readDay)
                 .set(data)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
                         Toast.makeText(CalendarCompanionActivity.this, "일정이 삭제되었습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-    }
-
-    public void saveDiary(String readDay) {
-        String content = contextEditText.getText().toString();
-
-        Map<String, Object> data = new HashMap<>();
-        data.put("diary", content);
-
-        firestore.collection("diaries").document(readDay)
-                .set(data)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(CalendarCompanionActivity.this, "일정이 저장되었습니다.", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
