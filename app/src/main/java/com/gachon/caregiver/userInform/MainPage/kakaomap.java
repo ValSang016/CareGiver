41498a280d2b09ea8f8aa0409a1728f82eb6c439
songmingyu <akdl0108@naver.com>
2023-06-04 06:10:25 +0900

    마커 클릭시 xmlㅇㅣ동

--- app/src/main/java/com/gachon/caregiver/userInform/MainPage/kakaomap.java
+++ app/src/main/java/com/gachon/caregiver/userInform/MainPage/kakaomap.java
@@ -35,7 +35,7 @@ import net.daum.mf.map.api.MapView;
 import java.util.HashMap;
 import java.util.Map;
 
-public class kakaomap extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapReverseGeoCoder.ReverseGeoCodingResultListener {
+public class kakaomap extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapReverseGeoCoder.ReverseGeoCodingResultListener, MapView.POIItemEventListener {
 
     private MapView mMapView;
     private String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION};
@@ -48,7 +48,6 @@ public class kakaomap extends AppCompatActivity implements MapView.CurrentLocati
 
     private boolean isLocationUpdated = false;
 
-
     private boolean checkLocationServicesStatus() {
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
@@ -56,7 +55,6 @@ public class kakaomap extends AppCompatActivity implements MapView.CurrentLocati
                 locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) &&
                 locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +66,7 @@ public class kakaomap extends AppCompatActivity implements MapView.CurrentLocati
 
         mMapView = findViewById(R.id.map_View);
         mMapView.setCurrentLocationEventListener(this);
+        mMapView.setPOIItemEventListener(this);
 
         if (!checkLocationServicesStatus()) {
             showDialogForLocationServiceSetting();
@@ -149,88 +148,127 @@ public class kakaomap extends AppCompatActivity implements MapView.CurrentLocati
         int hasFineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
 
         if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED) {
-            mMapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
-            return;
+            mMapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
+            mMapView.setShowCurrentLocationMarker(true);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
+                Toast.makeText(this, "위치 접근 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
         }
+    }
 
-        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSIONS_REQUEST_CODE && grantResults.length == REQUIRED_PERMISSIONS.length) {
+            boolean isAllGranted = true;
+
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    isAllGranted = false;
+                    break;
+                }
+            }
+
+            if (isAllGranted) {
+                mMapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
+                mMapView.setShowCurrentLocationMarker(true);
+            } else {
+                Toast.makeText(this, "위치 접근 권한을 허용해야 합니다.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 
     private void storeLocation() {
-        if (isLocationUpdated) {
-            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-            if (locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                final FirebaseUser user = mAuth.getCurrentUser();
-                if (user != null) {
-                    MapPoint currentLocation = mMapView.getMapCenterPoint();
-                    double latitude = currentLocation.getMapPointGeoCoord().latitude;
-                    double longitude = currentLocation.getMapPointGeoCoord().longitude;
-
-                    Map<String, Object> locationData = new HashMap<>();
-                    locationData.put("latitude", latitude);
-                    locationData.put("longitude", longitude);
-
-                    databaseReference.child("locations").child(user.getUid()).setValue(locationData)
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Toast.makeText(kakaomap.this, "위치 정보가 업로드되었습니다.", Toast.LENGTH_SHORT).show();
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(kakaomap.this, "위치 정보 업로드에 실패했습니다.", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                } else {
-                    Toast.makeText(kakaomap.this, "사용자 인증에 실패했습니다.", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(kakaomap.this, "GPS가 활성화되지 않았습니다.", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            Toast.makeText(kakaomap.this, "현재 위치를 업데이트 중입니다. 잠시 후에 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+        if (!isLocationUpdated) {
+            Toast.makeText(this, "현재 위치를 가져올 수 없습니다.", Toast.LENGTH_SHORT).show();
+            return;
         }
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            Toast.makeText(this, "사용자 인증에 실패했습니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String userId = currentUser.getUid();
+        String userName = currentUser.getDisplayName();
+        MapPoint currentLocation = mMapView.getMapCenterPoint();
+
+        MapPOIItem userMarker = new MapPOIItem();
+        userMarker.setItemName(userName);
+        userMarker.setTag(0);
+        userMarker.setMapPoint(currentLocation);
+        userMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        userMarker.setCustomImageResourceId(R.drawable.ic_marker);
+        userMarker.setCustomImageAutoscale(false);
+        userMarker.setCustomImageAnchor(0.5f, 1.0f);
+        userMarker.setUserObject(userId);
+
+        mMapView.addPOIItem(userMarker);
+        mMapView.selectPOIItem(userMarker, true);
     }
 
     private void getSavedLocations() {
-        final FirebaseUser user = mAuth.getCurrentUser();
-        if (user != null) {
-            DatabaseReference locationsRef = databaseReference.child("locations");
-            locationsRef.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    // Iterate over each child node under "locations"
-                    for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                        String userId = userSnapshot.getKey();
-                        if (!userId.equals(user.getUid())) {
-                            // Retrieve latitude and longitude values from the user's location data
-                            Double latitude = userSnapshot.child("latitude").getValue(Double.class);
-                            Double longitude = userSnapshot.child("longitude").getValue(Double.class);
+        DatabaseReference locationsRef = databaseReference.child("locations");
+        locationsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mMapView.removeAllPOIItems();
 
-                            if (latitude != null && longitude != null) {
-                                // Create a marker for the user's location
-                                MapPOIItem marker = new MapPOIItem();
-                                marker.setItemName("User Location");
-                                marker.setTag(0);
-                                marker.setMapPoint(MapPoint.mapPointWithGeoCoord(latitude, longitude));
-                                marker.setMarkerType(MapPOIItem.MarkerType.RedPin);
+                for (DataSnapshot locationSnapshot : snapshot.getChildren()) {
+                    String userId = locationSnapshot.getKey();
+                    double latitude = locationSnapshot.child("latitude").getValue(Double.class);
+                    double longitude = locationSnapshot.child("longitude").getValue(Double.class);
 
-                                mMapView.addPOIItem(marker);
-                                mMapView.selectPOIItem(marker, true);
-                            }
-                        }
-                    }
+                    MapPoint location = MapPoint.mapPointWithGeoCoord(latitude, longitude);
+
+                    MapPOIItem marker = new MapPOIItem();
+                    marker.setItemName(userId);
+                    marker.setTag(1);
+                    marker.setMapPoint(location);
+                    marker.setMarkerType(MapPOIItem.MarkerType.RedPin);
+                    marker.setSelectedMarkerType(MapPOIItem.MarkerType.BluePin);
+
+                    mMapView.addPOIItem(marker);
                 }
+            }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    Toast.makeText(kakaomap.this, "위치 정보를 가져오는 데 실패했습니다.", Toast.LENGTH_SHORT).show();
-                }
-            });
-        } else {
-            Toast.makeText(kakaomap.this, "사용자 인증에 실패했습니다.", Toast.LENGTH_SHORT).show();
-        }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Empty implementation
+            }
+        });
+    }
+
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+        // Empty implementation
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+        String userId = (String) mapPOIItem.getUserObject();
+
+        // Pass the UID to the mapcomp_info activity
+        Intent intent = new Intent(kakaomap.this, mapcomp_info.class);
+        intent.putExtra("id", userId);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+        // Empty implementation
+    }
+
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+        // Empty implementation
     }
 }
