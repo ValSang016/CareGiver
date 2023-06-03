34de185bacc6eeba3aeb8bd39ee742b707ffa320
yuheun <youheun422@gmail.com>
2023-06-03 18:54:07 +0900

    내정보_보호자 xml, java 추가

---
+++ app/src/main/java/com/gachon/caregiver/userInform/MainPage/userInfoPage_parents.java
@@ -0,0 +1,35 @@
+package com.gachon.caregiver.userInform.MainPage;
+
+import static android.content.ContentValues.TAG;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gachon.caregiver.R;
+
+public class userInfoPage_parents extends AppCompatActivity {
+
+    private EditText name;
+    private EditText old;
+    private EditText address;
+    private EditText special;
+
+
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.myinformation_companion);
+
+        name = findViewById(R.id.name);
+        old = findViewById(R.id.age);
+        address = findViewById(R.id.address);
+        special = findViewById(R.id.significant);
+
+
+    }
+}
