3102499554090b35bc2223cc6f0936dd9611c861
parkjonghyun <hynmail@naver.com>
2023-06-04 02:37:21 +0900

    일단 연결했는데 민규형데베 끝나면 연결 가능

--- app/src/main/java/com/gachon/caregiver/userInform/MainPage/mapcomp_info.java
+++ app/src/main/java/com/gachon/caregiver/userInform/MainPage/mapcomp_info.java
@@ -1,6 +1,11 @@
 package com.gachon.caregiver.userInform.MainPage;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class mapcomp_info extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
 }
